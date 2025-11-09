5ff2e9c62a1b6f81de26495136198f6b91483bcc
abhayjajodia87-lab <abhayjajodia87@gmail.com>
2025-11-09 14:53:48 +0200

    Add files via upload

---
+++ static/js/logout.js
@@ -0,0 +1,16 @@
+// Simple logout function for beginners
+function logout() {
+    // Send a request to the server to end the session
+    fetch('/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            // If logout successful, redirect to login page
+            window.location.href = '/login';
+        }
+    });
+}
